6fecff6df1f4186d3f20f80eaa50785344c079ba
Utkan Güngördü <salviati@freeconsole.org>
2012-02-27 19:59:36 +0900

    add reader

---
+++ lzss/reader.go
@@ -0,0 +1,31 @@
+package lzss
+
+import (
+	"bytes"
+	"io"
+	"io/ioutil"
+)
+
+func NewCustomReader(r io.Reader, order Order, flagFunc FlagFuncType, referenceFunc ReferenceFuncType, threshold int) (io.Reader, error) {
+	d, err := NewCustomDecoder(order, flagFunc, referenceFunc, threshold)
+	if err != nil {
+		return nil, err
+	}
+	
+	in, err := ioutil.ReadAll(r)
+	if err != nil {
+		return nil, err
+	}
+
+	out, err := d.Decode(in)
+	if err != nil {
+		return nil, err
+	}
+	
+	return bytes.NewBuffer(out), nil
+}
+
+func NewReader(r io.Reader, order Order) (io.Reader, error) {
+	return NewCustomReader(r, order, DefaultFlagFunc, DefaultReferenceFunc, DefaultThreshold)
+}
+
